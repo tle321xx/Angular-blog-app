749e461a5c8279cef1c39fd2149d543e2a5dda1c
tle321xx <t.wachirawit28@gmail.com>
2024-01-10 19:35:13 +0700

    design app

---
+++ src/app/pages/term-and-condition/term-and-condition.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-term-and-condition',
+  templateUrl: './term-and-condition.component.html',
+  styleUrl: './term-and-condition.component.css'
+})
+export class TermAndConditionComponent {
+
+}
